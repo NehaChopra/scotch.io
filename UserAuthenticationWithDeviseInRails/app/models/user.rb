2d54e0749ca57f67d7a70768027d1833fbc2cb5a
neha <neha.chopra@navyuginfo.com>
2017-12-22 06:37:37 +0530

    User Authentication

---
+++ UserAuthenticationWithDeviseInRails/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessor :firstname, :lastname, :email, :password, :password_confirmation, :remember_me
+end
